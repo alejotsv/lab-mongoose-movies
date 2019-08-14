529fda6cf3b9fa9657eb0138c5303e78a162bbb0
Alejandro Salas <asalas.wd@outlook.com>
2019-08-13 23:55:32 -0400

    complete celebrity iteration

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -12,6 +12,18 @@ router.get('/celebrities', (req, res, next) => {
     .catch(err => console.log("Error while getting the celebrities from the DB: ", err));    
 });
 
+// router to display a form to create a new celebrity
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('new-celebrity');
+});
+
+// router to post a celebrity to the database
+router.post('/celebrities/create', (req, res, next) => {
+  Celebrity
+    .create(req.body)
+    .then(newCelebrity => res.redirect('/celebrities'))
+    .catch(err => console.log("Error while creating new celebrity: ", err));
+});
 
 // in order to use routes anywhere else in this application, we haeve to export them
 module.exports = router;
